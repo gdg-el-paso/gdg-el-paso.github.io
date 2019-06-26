71035700a8c607182847bb73fd9e881dfa747177
Bryan Ramos <ramos.bryan100@gmail.com>
2019-06-25 23:26:43 -0700

    Add new build

---
+++ build/precache-manifest.d1eb5a908cf26e7f27da844fd62630a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb95c7d9b70b5059d67e7129491521cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1207fde7f97baa3c6bac",
+    "url": "/static/css/main.1ac88f0c.chunk.css"
+  },
+  {
+    "revision": "2d22601409ae9179d98f",
+    "url": "/static/js/2.4dcf69e0.chunk.js"
+  },
+  {
+    "revision": "1207fde7f97baa3c6bac",
+    "url": "/static/js/main.f02ead34.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "079918807926abc9d9e06a847917cfe5",
+    "url": "/static/media/androidRobot.07991880.svg"
+  },
+  {
+    "revision": "2ff00cfc86426f9dab83af5aa82127d1",
+    "url": "/static/media/logo.2ff00cfc.png"
+  }
+]);
